1ece7f6736801e7f3beca4946b0c91e9bdc84660
Josh Nussbaum <joshua.nussbaum@shopify.com>
2017-09-22 21:32:43 -0400

    Refactor parsing to remove branching

--- lib/statsd/parser.ex
+++ lib/statsd/parser.ex
@@ -3,27 +3,24 @@ defmodule StatsD.Parser do
     defexception [:message]
   end
 
+  @types %{
+    "c" => :counter,
+    "g" => :guage,
+    "s" => :set,
+    "ms" => :timing
+  }
+
   def parse(text) do
     try do
       [bucket, rest] = String.split(text, ":")
       parts = String.split(rest, "|")
-      type = List.last(parts)
+      type_code = List.last(parts)
+      type = @types[type_code]
+      value = parts |> hd |> String.to_integer
 
-      do_parse(type, bucket, Enum.drop(parts, -1))
+      {type, bucket, value}
     rescue
       _ -> raise Error, "Failed to parse `#{text}`"
     end
   end
-
-  defp do_parse("c", bucket, [value]),
-    do: {:counter, bucket, String.to_integer(value)}
-
-  defp do_parse("g", bucket, [value]),
-    do: {:guage, bucket, String.to_integer(value)}
-
-  defp do_parse("s", bucket, [value]),
-    do: {:set, bucket, String.to_integer(value)}
-
-  defp do_parse("ms", bucket, [value]),
-    do: {:timing, bucket, String.to_integer(value)}
 end
